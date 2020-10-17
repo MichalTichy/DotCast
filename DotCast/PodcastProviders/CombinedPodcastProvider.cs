5aa9468e3a3f8f8876febf62c56004dfe6708dba
Michal Tichý <email@tichymichal.net>
2020-10-17 15:45:16 +0200

    added combined podcast provider

---
+++ DotCast/PodcastProviders/CombinedPodcastProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotCast
+{
+    public class CombinedPodcastProvider : IPodcastProvider
+    {
+        private readonly ICollection<IPodcastProvider> providers;
+
+        public CombinedPodcastProvider(ICollection<IPodcastProvider> providers)
+        {
+            this.providers = providers;
+        }
+        public Feed GetFeed(string podcastName)
+        {
+            return providers.Select(t => t.GetFeed(podcastName)).SingleOrDefault(t => t != null);
+        }
+
+        public IEnumerable<string> GetPodcastNames()
+        {
+            var podcastNames = providers.SelectMany(t=>t.GetPodcastNames()).ToArray();
+            if (podcastNames.Length != podcastNames.Distinct().Count())
+            {
+                throw new ArgumentException("Two podcast with the same name exist in multiple sources.");
+            }
+
+            return podcastNames.OrderBy(t => t);
+        }
+    }
+}
