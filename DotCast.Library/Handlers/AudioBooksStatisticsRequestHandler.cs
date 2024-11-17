4b24a43a3084966193d70be32414cb0be6bdaf3f
Michal Tichý <email@tichymichal.net>
2024-11-17 12:01:04 +0100

    added error handling

--- DotCast.Library/Handlers/AudioBooksStatisticsRequestHandler.cs
+++ DotCast.Library/Handlers/AudioBooksStatisticsRequestHandler.cs
@@ -1,16 +1,26 @@
 ﻿using DotCast.Library.Specifications;
 using DotCast.SharedKernel.Messages;
 using DotCast.SharedKernel.Models;
+using Microsoft.Extensions.Logging;
 using Shared.Infrastructure.Persistence.Repositories;
 
 namespace DotCast.Library.Handlers
 {
-    public class AudioBooksStatisticsRequestHandler(IReadOnlyRepository<AudioBook> repository) : IMessageHandler<AudioBooksStatisticsRequest, AudioBooksStatistics>
+    public class AudioBooksStatisticsRequestHandler
+        (IReadOnlyRepository<AudioBook> repository, ILogger<AudioBooksStatisticsRequestHandler> logger) : IMessageHandler<AudioBooksStatisticsRequest, AudioBooksStatistics>
     {
         public async Task<AudioBooksStatistics> Handle(AudioBooksStatisticsRequest message)
         {
-            var specification = new AudioBookStatisticSpecification();
-            return await repository.GetBySpecAsync(specification) ?? new AudioBooksStatistics(0, 0, TimeSpan.Zero);
+            try
+            {
+                var specification = new AudioBookStatisticSpecification();
+                return (await repository.GetBySpecAsync(specification))!;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Unable to retrieve statistics.");
+                return new AudioBooksStatistics(0, 0, TimeSpan.Zero);
+            }
         }
     }
 }
